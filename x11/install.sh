173875609bd50c3e61f9bb17499ec7c402ee3b6c
Justin Chadwell <jedevc@gmail.com>
2018-10-11 14:19:12 +0100

    Actually install the x11 subfolders

--- x11/install.sh
+++ x11/install.sh
@@ -6,6 +6,6 @@ DESTINATION="$1"
 cp $SOURCE/xinitrc $DESTINATION/.xinitrc
 
 mkdir -p $DESTINATION/.config
-cp -r $SOURCE/i3 $DESTINATION/.config/i3
-cp -r $SOURCE/rofi $DESTINATION/.config/rofi
-cp -r $SOURCE/termite $DESTINATION/.config/termite
+cp -r $SOURCE/i3 $DESTINATION/.config
+cp -r $SOURCE/rofi $DESTINATION/.config
+cp -r $SOURCE/termite $DESTINATION/.config
