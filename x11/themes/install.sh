ba2ab26ff692a77e8dfa8b923a8494c961b53cd1
Justin Chadwell <jedevc@gmail.com>
2018-10-12 15:57:16 +0100

    Add termite padding

--- x11/themes/install.sh
+++ x11/themes/install.sh
@@ -7,6 +7,7 @@ cp $SOURCE/gtk-2.0 $DESTINATION/.gtkrc-2.0
 
 mkdir -p $DESTINATION/.config/gtk-3.0
 cp $SOURCE/gtk-3.0 $DESTINATION/.config/gtk-3.0/settings.ini
+cp $SOURCE/gtk-3.0.css $DESTINATION/.config/gtk-3.0/gtk.css
 
 mkdir -p $DESTINATION/.config/qt5ct
 cp $SOURCE/qt5ct.conf $DESTINATION/.config/qt5ct/qt5ct.conf
