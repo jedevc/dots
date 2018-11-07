07b55af8c9aeb896fa5782ee493fcf0b97ac4645
Justin Chadwell <jedevc@gmail.com>
2018-11-07 11:33:11 +0000

    Use vim-plug for plugins

--- vim/install.sh
+++ vim/install.sh
@@ -6,3 +6,7 @@ DESTINATION="$1"
 cp $SOURCE/vimrc $DESTINATION/.vimrc
 mkdir -p $DESTINATION/.vim/swap
 mkdir -p $DESTINATION/.vim/undo
+
+# plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -c 'PlugInstall|qall'
