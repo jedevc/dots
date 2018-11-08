c069c95339bc3fe4a82ecc847612f6830ce5abd7
Justin Chadwell <jedevc@gmail.com>
2018-11-08 09:40:08 +0000

    Fix install script for vim

--- vim/install.sh
+++ vim/install.sh
@@ -4,9 +4,9 @@ SOURCE=`dirname $0`
 DESTINATION="$1"
 
 cp $SOURCE/vimrc $DESTINATION/.vimrc
-mkdir -p $DESTINATION/.vim
-mkdir $DESTINATION/.vim/swap
-mkdir $DESTINATION/.vim/undo
+mkdir -p $DESTINATION/.vim/swap
+mkdir -p $DESTINATION/.vim/undo
+cp -r $SOURCE/colors $DESTINATION/.vim
 
 # plugins
 curl -fLo $DESTINATION/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
