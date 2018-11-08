3e8dc3e5d78f20a2098714b45df788b24075bca5
Justin Chadwell <jedevc@gmail.com>
2018-11-08 08:38:56 +0000

    Fix vim installation paths

--- vim/install.sh
+++ vim/install.sh
@@ -4,9 +4,10 @@ SOURCE=`dirname $0`
 DESTINATION="$1"
 
 cp $SOURCE/vimrc $DESTINATION/.vimrc
-mkdir -p $DESTINATION/.vim/swap
-mkdir -p $DESTINATION/.vim/undo
+mkdir -p $DESTINATION/.vim
+mkdir $DESTINATION/.vim/swap
+mkdir $DESTINATION/.vim/undo
 
 # plugins
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo $DESTINATION/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c 'PlugInstall|qall'
