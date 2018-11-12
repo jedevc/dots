fb993026c55fe0d22e592a0470aa35729acdc189
Justin Chadwell <jedevc@gmail.com>
2018-11-12 22:20:21 +0000

    Fix installation stuff for vim

--- vim/install.sh
+++ vim/install.sh
@@ -9,5 +9,8 @@ mkdir -p $DESTINATION/.vim/undo
 cp -r $SOURCE/colors $DESTINATION/.vim
 
 # plugins
-curl -fLo $DESTINATION/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+PLUG="$DESTINATION/.vim/autoload/plug.vim"
+if [ ! -f $PLUG ]; then
+	curl -fLo $PLUG --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 vim -c 'PlugInstall|qall'
