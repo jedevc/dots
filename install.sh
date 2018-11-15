d255e248fa54cf839dd70cb309b4c4676d098523
Justin Chadwell <jedevc@gmail.com>
2018-11-15 11:54:54 +0000

    Better installer

--- install.sh
+++ install.sh
@@ -1,28 +1,62 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
+# help message
+if [ $# -ge 1 ] && [[ $1 == '-h' ]]; then
+	echo "Usage: $0 [install path] [components]"
+	exit
+fi
+
+# destination
+if [ -z $1 ]; then
 	DESTINATION=$HOME
 else
-	DESTINATION="$1"
+	DESTINATION=$1
 	mkdir -p $DESTINATION
 fi
 
-echo "Installing to $DESTINATION."
+# components
+if [ -z $2 ]; then
+	COMPONENTS=all
+else
+	COMPONENTS=${@:2}
+fi
 
-echo "Installing scripts..."
-cp -r bin/ $DESTINATION
+# components checker
+function shouldInstall {
+	if [[ $COMPONENTS == all ]]; then
+		return 0
+	fi
 
-echo "Installing zsh..."
-./zsh/install.sh $DESTINATION
+	target=$1
+	for i in $COMPONENTS; do
+		if [[ $i == $target ]]; then
+			return 0
+		fi
+	done
 
-echo "Installing vim..."
-./vim/install.sh $DESTINATION
+	return 1
+}
 
-echo "Installing x11..."
-./x11/install.sh $DESTINATION
+# actual install
+echo "Installing '$COMPONENTS' to $DESTINATION."
+if shouldInstall scripts; then
+	echo "Installing scripts..."
+	cp -r bin/ $DESTINATION
+fi
+if shouldInstall zsh; then
+	echo "Installing zsh..."
+	./zsh/install.sh $DESTINATION
+fi
+if shouldInstall vim; then
+	echo "Installing vim..."
+	./vim/install.sh $DESTINATION
+fi
+if shouldInstall x11; then
+	echo "Installing x11..."
+	./x11/install.sh $DESTINATION
 
-echo "Installing misc..."
-cp wallpaper-temple.png $DESTINATION/.wallpaper.png
-feh --bg-scale $DESTINATION/.wallpaper.png
-
+	echo "> Setting background..."
+	cp wallpaper-temple.png $DESTINATION/.wallpaper.png
+	feh --bg-scale $DESTINATION/.wallpaper.png
+fi
 echo "Done."
