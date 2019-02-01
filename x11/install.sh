63344f26ab81437a14e1a6b25d2704d2f9daea5b
Justin Chadwell <jedevc@gmail.com>
2019-02-01 11:23:49 +0000

    Add zathurarc

--- x11/install.sh
+++ x11/install.sh
@@ -10,6 +10,7 @@ mkdir -p $DESTINATION/.config
 cp $SOURCE/compton.conf $DESTINATION/.config
 cp -r $SOURCE/i3 $DESTINATION/.config
 cp -r $SOURCE/rofi $DESTINATION/.config
+cp -r $SOURCE/zathura $DESTINATION/.config
 cp -r $SOURCE/termite $DESTINATION/.config
 cp -r $SOURCE/polybar $DESTINATION/.config
 
