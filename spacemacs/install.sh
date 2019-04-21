d4c5c766e5f1da7e46e8bb8b2d9c403d081340a3
Justin Chadwell <jedevc@gmail.com>
2019-04-21 16:00:16 +0100

    Fira code download

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -7,3 +7,14 @@ mkdir -p $DESTINATION/.spacemacs.d/
 cp $SOURCE/*.el $DESTINATION/.spacemacs.d/
 
 mkdir -p $DESTINATION/.spacemacs.d/layers
+
+if [[ ! -f $DESTINATION/.local/share/fonts/FiraCode-Regular-Symbol.otf ]]; then
+	pushd `mktemp -d`
+
+	wget "https://github.com/tonsky/FiraCode/files/412440/FiraCode-Regular-Symbol.zip"
+	unzip FiraCode-Regular-Symbol.zip
+	mkdir -p $DESTINATION/.local/share/fonts
+	mv FiraCode-Regular-Symbol.otf $DESTINATION/.local/share/fonts
+
+	popd
+fi
