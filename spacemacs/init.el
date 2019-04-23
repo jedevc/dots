33f942fdbe9c237e6103a80bb0207a6990e34c50
Justin Chadwell <jedevc@gmail.com>
2019-04-23 22:34:55 +0100

    Change spacemacs default font

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -144,11 +144,11 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Fira Code"
+   dotspacemacs-default-font '("Hack"
                                :size 14
                                :weight normal
                                :width normal
-                               :powerline-scale 1.7)
+                               :powerline-scale 1.8)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; The leader key
