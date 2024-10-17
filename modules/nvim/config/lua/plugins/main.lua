24641a619fc993060b837efbfd588a55ff6e918e
Justin Chadwell <me@jedevc.com>
2024-10-17 13:12:43 +0100

    chore: swap root/cwd bindings
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- modules/nvim/config/lua/plugins/main.lua
+++ modules/nvim/config/lua/plugins/main.lua
@@ -22,6 +22,12 @@ return {
         },
       },
     },
+    keys = {
+      -- swap cwd/root bindings
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>fF", LazyVim.pick("files", { root = true }), desc = "Find Files (root)" },
+    },
   },
 
   {
