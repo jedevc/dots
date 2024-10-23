4bd5b9f9649dd58a6d4c85076cd99b9295a526d2
Justin Chadwell <me@jedevc.com>
2024-10-23 11:27:02 +0100

    chore: update terminal default keymap to root
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- modules/nvim/config/lua/config/keymaps.lua
+++ modules/nvim/config/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- swap cwd/root bindings
+local lazyterm = function()
+  LazyVim.terminal()
+end
+vim.keymap.set("n", "<c-/>", lazyterm, { desc = "Terminal (cwd)" })
+vim.keymap.set("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
