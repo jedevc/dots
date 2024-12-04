846683ad236a9ec32a2b0251ce80408ef8269466
Justin Chadwell <me@jedevc.com>
2024-12-04 11:32:55 +0000

    add rust support
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- modules/nvim/config/lua/plugins/main.lua
+++ modules/nvim/config/lua/plugins/main.lua
@@ -2,6 +2,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
 
   { import = "lazyvim.plugins.extras.test.core" },
 
