898ec301f32ad93c4893406b79ed6ead097026e6
Justin Chadwell <me@jedevc.com>
2024-12-16 13:25:05 +0000

    Add support for nvim markdown
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- modules/nvim/config/lua/plugins/main.lua
+++ modules/nvim/config/lua/plugins/main.lua
@@ -3,6 +3,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
 
   { import = "lazyvim.plugins.extras.test.core" },
 
