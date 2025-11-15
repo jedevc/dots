975ee093c01737194cdc8e0e4fb74cbc7460de6a
Justin Chadwell <me@jedevc.com>
2025-11-15 22:59:23 +0000

    chore: avoid hidden hxml
    Signed-off-by: Justin Chadwell <me@jedevc.com>

---
+++ modules/nvim/config/lua/plugins/haxe.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "haxe-language-server" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        haxe_language_server = {
+          cmd = { "haxe-language-server" },
+          filetypes = { "haxe" },
+          root_markers = { ".hxml", ".git", "Project.xml" },
+          before_init = function(params, config)
+            local hxmls = vim.fs.find(function(name, path)
+              -- only consider .hxml files
+              if not name:match("%.hxml$") then
+                return false
+              end
+              -- ignore hidden directories
+              for dir in path:gmatch("[^/]+") do
+                if dir:sub(1, 1) == "." then
+                  return false
+                end
+              end
+              return true
+            end, { path = config.root_dir, type = "file" })
+            local hxml = hxmls[1]
+            if hxml then
+              params.initializationOptions = params.initializationOptions or {}
+              params.initializationOptions.displayArguments = { hxml }
+            end
+          end,
+          settings = {
+            haxe = {
+              buildCompletionCache = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- see autocmds.lua for where this is installed
+      ensure_installed = { "haxe" },
+    },
+  },
+}
